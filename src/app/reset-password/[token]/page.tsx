12d5a1aac95f025f0206d3b48a67db1f28fd4bc6
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2025-02-28 08:23:09 -0100

    new commit

--- src/app/reset-password/[token]/page.tsx
+++ src/app/reset-password/[token]/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+// app/reset-password/[token]/page.tsx
+import React from 'react';
+import ResetPasswordClient from './ResetPasswordClient';
 
-const page = () => {
-  return (
-    <div></div>
-  )
+export default function Page({
+  params,
+  
+}: {
+  params: { token: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}): React.ReactElement {
+  return <ResetPasswordClient token={params.token} />;
 }
-
-export default page
